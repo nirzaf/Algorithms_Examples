c05e0b39e112044840147f7a9977394e969c5705
franekfranek <franciszekezawadzki@gmail.com>
2020-07-13 13:00:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Algorithm_A_Day.BinarySearch;
+using System;
+
+namespace Algorith_A_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testArr = new int[] { 10, 2, 5, 6, 8, 9 };
+
+            NumberOfRoatations numberOfRoatations = new NumberOfRoatations();
+            Console.WriteLine(numberOfRoatations.NumberOfRotation(testArr));
+        }
+    }
+}
