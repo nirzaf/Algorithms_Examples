a1f330896ef7d2134560b09c53fee6446ff4f9e2
franekfranek <franciszekezawadzki@gmail.com>
2020-11-13 12:11:20 +0100

    Running_Sum_of1d_Array_LC_1480_E, Kids_With_Candies_LC_1431_E

---
+++ Arrays/Kids_With_Candies_LC_1431_E.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm_A_Day.Arrays
+{
+    public class Kids_With_Candies_LC_1431_E
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            List<bool> result = new List<bool>();
+            if (candies.Length == 0 || candies == null || extraCandies < 0) return result;
+
+            int maxVal = candies.Max();
+
+            foreach (int can in candies)
+            {
+                if (can + extraCandies >= maxVal)
+                {
+                    result.Add(true);
+                }
+                else
+                {
+                    result.Add(false);
+                }
+            }
+            return result;
+        }
+
+        public IList<bool> KidsWithCandies2(int[] candies, int extraCandies)
+        {
+            var max = candies.Max();
+            return candies.Select(c => c + extraCandies >= max).ToList();
+        }
+
+    }
+}
---
+++ Arrays/Running_Sum_of1d_Array_LC_1480_E.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm_A_Day.Arrays
+{
+    public class Running_Sum_of1d_Array_LC_1480_E
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            if (nums.Length == 0 || nums == null) return new int[] { };
+            int len = nums.Length;
+
+            for (int i = 1; i < len; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+            return nums;
+        }
+    }
+}
