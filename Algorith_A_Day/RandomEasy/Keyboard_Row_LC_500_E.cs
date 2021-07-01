ecdd0a06214bc3aee8144894b1895f0215d00522
franekfranek <franciszekezawadzki@gmail.com>
2021-07-02 00:39:41 +0200

    Subtract_Integer_1281_LC_E  + js

--- Algorith_A_Day/RandomEasy/Keyboard_Row_LC_500_E.cs
+++ Algorith_A_Day/RandomEasy/Keyboard_Row_LC_500_E.cs
@@ -23,9 +23,7 @@ namespace Algorithm_A_Day.RandomEasy
             {
                 bool r1 = true;
                 bool r2 = true;
-                bool r3 = true;
-
-                
+                bool r3 = true;           
 
                 for (int i = 0; i < word.Length; i++)
                 {
@@ -60,5 +58,115 @@ namespace Algorithm_A_Day.RandomEasy
             return result.ToArray();
 
         }
+
+        public string[] FindWords2(string[] words)
+        {
+            var result = new List<string>();
+
+            if (words.Length == 0) return result.ToArray();
+
+            var keyboard = new HashSet<char>[3];
+            keyboard[0] = new HashSet<char>() { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' };
+            keyboard[1] = new HashSet<char>() { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' };
+            keyboard[2] = new HashSet<char>() { 'z', 'x', 'c', 'v', 'b', 'n', 'm' };
+
+
+
+            foreach (var word in words)
+            {
+                if (word == "") continue;
+                var curRow = -1;
+
+                foreach (var c in word)
+                {
+                    if (curRow == -1)
+                    {
+                        for (int i = 0; i < keyboard.Length; i++)
+                        {
+                            if (keyboard[i].Contains(char.ToLower(c)))
+                            {
+                                if (curRow == -1) curRow = i;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (!keyboard[curRow].Contains(char.ToLower(c)))
+                        {
+                            curRow = -1;
+                            break;
+                        }
+                    }
+                }
+
+                if (curRow != -1)
+                {
+                    result.Add(word);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+
+        /*
+         
+         var findWords = function(words) {
+            return words.filter(word =>{
+            let presentInTopRow = word.split('').every(c=>"qwertyuiop".indexOf(c.toLowerCase())>-1);
+            let presentInTMidRow = word.split('').every(c=>"asdfghjkl".indexOf(c.toLowerCase())>-1);
+            let presentInTBottomRow = word.split('').every(c=>"zxcvbnm".indexOf(c.toLowerCase())>-1);
+        return presentInTopRow||presentInTMidRow||presentInTBottomRow;
+    });
+};
+         
+         var findWords2 = function(words) {
+    var keyMap = [
+        {
+            q: 'q', w: 'w', e: 'e', r: 'r', t: 't', y: 'y',
+            u: 'u', i: 'i', o: 'o', p: 'p'
+        
+        },
+        {
+            a: 'a', s: 's', d: 'd', f: 'f', g: 'g', h: 'h',
+            j: 'j', k: 'k', l:'l'
+        },
+        {
+            z: 'z', x: 'x', c: 'c', v: 'v', b: 'b', n: 'n', m: 'm'
+        }
+    ];
+    
+    var row = '';
+    var output = [];
+    
+    for (var i = 0; i < words.length; i++) {
+        for (var j = 0; j < words[i].length; j++) {
+            var char = words[i][j].toLowerCase();
+            
+            if (keyMap[0][char]) {
+                row += '0';
+            }
+            if (keyMap[1][char]) {
+                row += '1';
+            }
+                
+            if (keyMap[2][char]) {
+                row += '2';
+            }
+        }
+        
+        if (checkRow(row.split(''))) {
+            output.push(words[i]);
+        }
+        row = '';
+    }
+    
+    return output;
+};
+
+const checkRow = arr => arr.every( v => v === arr[0] );
+         
+         
+         */
     }
 }
---
+++ Algorith_A_Day/RandomEasy/Subtract_Integer_1281_LC_E.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_A_Day.RandomEasy
+{
+    public class Subtract_Integer_1281_LC_E
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            if (n <= 0) return - 1;
+
+            int multi = 1;
+            int addi = 0;
+
+            foreach (char c in n.ToString())
+            {
+                multi *= int.Parse(c.ToString());
+                addi += int.Parse(c.ToString());
+            }
+
+            return multi - addi;
+        }
+
+
+        //suprisngly fast but SC not too good
+        /*
+        var subtractProductAndSum = function(n) {
+            m = n.toString().split('');
+
+            addi = m.reduce((total, cur) => total + parseInt(cur), 0);
+            multi = m.reduce((total, cur) => total * parseInt(cur), 1);
+    
+            return multi - addi;
+        };
+
+
+        var subtractProductAndSum = function(n) {
+            return [...String(n)].reduce((a,c) => a * Number(c)) - [...String(n)].reduce((a,c) => a + Number(c), 0)
+        };????
+
+        var subtractProductAndSum = function(n) {
+    let product = 1, sum = 0;
+    while (n > 0) {
+        let lastDigit = n % 10;
+        product *= lastDigit;
+        sum += lastDigit;
+        n = Math.floor(n / 10);
+    }
+    return product - sum;
+};
+         */
+    }
+}
